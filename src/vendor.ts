adaf0e9bffe62b843a70fd64db1ed2a4e24ab369
Chuoxian Yang <yangchuoxian.appdev@gmail.com>
2016-06-21 14:56:28 +0800

    Initial import

---
+++ src/vendor.ts
@@ -0,0 +1,26 @@
+// Angular 2
+import '@angular/platform-browser'
+import '@angular/platform-browser-dynamic'
+import '@angular/core'
+import '@angular/common'
+import '@angular/http'
+import '@angular/router-deprecated'
+import '@angular2-material/button'
+import '@angular2-material/card'
+import '@angular2-material/checkbox'
+// Angular2 material
+import '@angular2-material/grid-list'
+import '@angular2-material/icon'
+import '@angular2-material/input'
+import '@angular2-material/list'
+import '@angular2-material/progress-bar'
+import '@angular2-material/progress-circle'
+import '@angular2-material/radio'
+import '@angular2-material/sidenav'
+import '@angular2-material/slide-toggle'
+import '@angular2-material/tabs'
+import '@angular2-material/toolbar'
+// RxJS
+import 'rxjs'
+// Other vendors for example jQuery, Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
